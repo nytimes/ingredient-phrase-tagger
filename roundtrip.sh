ba08548041aacb3ed9ec70e1aa200d275bf31e95
dexteradeus <dexteradeus@users.noreply.github.com>
2016-07-23 15:46:00 -0400

    Add command line options to roundtrip.sh

--- roundtrip.sh
+++ roundtrip.sh
@@ -2,6 +2,35 @@
 COUNT_TRAIN=20000
 COUNT_TEST=2000
 
+usage() {
+    printf "Usage: %s: [−-train-count COUNT] [−test-count COUNT]\n" $0
+    exit 2
+}
+
+optspec=":h-:"
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            case "${OPTARG}" in
+                "train-count")
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    COUNT_TRAIN=$val
+                    ;;
+                "test-count")
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    COUNT_TEST=$val
+                    ;;
+                *)
+                    usage;;
+            esac;;
+        *)
+            usage;;
+    esac
+done
+
+echo "Training count: ${COUNT_TRAIN}"
+echo "Testing count: ${COUNT_TEST}"
+
 echo "generating training data..."
 bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TRAIN --offset=0 > tmp/train_file || exit 1
 
