5087827e7f060ee64c6454b905704cc404569970
dexteradeus <dexteradeus@users.noreply.github.com>
2016-07-23 18:45:25 -0400

    Use a threading pool when generating training data
    - Replace generate data for loop with a pool of worker processes for
      processing each line of the training input file in a separate process
    - Move writing the output data to a seperate process which reads from
      the queue filled by the worker processes

--- ingredient_phrase_tagger/training/cli.py
+++ ingredient_phrase_tagger/training/cli.py
@@ -4,6 +4,8 @@ import re
 import decimal
 import argparse
 import pandas as pd
+from multiprocessing import Pool, TimeoutError, Queue, Process, Manager
+from Queue import Empty
 
 import utils
 
@@ -12,11 +14,13 @@ class Cli(object):
     def __init__(self, argv):
         self.opts = self._parse_args(argv)
         self._upstream_cursor = None
+        m = Manager()
+        self.output_queue = m.Queue()
 
     def run(self):
-        self.generate_data(self.opts.count, self.opts.offset)
+        self.generate_data(self.opts.count, self.opts.offset, self.opts.threads)
 
-    def generate_data(self, count, offset):
+    def generate_data(self, count, offset, threads):
         """
         Generates training data in the CRF++ format for the ingredient
         tagging task
@@ -29,7 +33,17 @@ class Cli(object):
 
         df_slice = df.iloc[start: end]
 
-        for index, row in df_slice.iterrows():
+        qr = Process(target=self.start_queue_reader)
+        qr.start()
+        worker_pool = Pool(processes=threads or None)
+        worker_pool.map_async(self._generate_data_worker, df_slice.iterrows())
+        worker_pool.close()
+        worker_pool.join()
+        self.output_queue.put('DONE')
+        qr.join()
+
+    def _generate_data_worker(self, args):
+            index, row = args
             out = []
             try:
                 # extract the display name
@@ -48,7 +62,17 @@ class Cli(object):
                 pass
 
             if out:
-                print('\n'.join(out), end='\n\n')
+                self.output_queue.put('\n'.join(out))
+
+    def start_queue_reader(self):
+        o = None
+        while o != 'DONE':
+            try:
+                o = self.output_queue.get()
+                if o != 'DONE':
+                    print(o, end="\n\n", flush=True)
+            except Empty:
+                pass
 
     def parseNumbers(self, s):
         """
@@ -161,6 +185,7 @@ class Cli(object):
 
         parser.add_argument("--count", default=100, type=int, help=' ')
         parser.add_argument("--offset", default=0, type=int, help=' ')
+        parser.add_argument("--threads", default=0, type=int, help=' ')
         parser.add_argument("--data-path", default="nyt-ingredients-snapshot-2015.csv", help=' ')
 
         return parser.parse_args(argv)
