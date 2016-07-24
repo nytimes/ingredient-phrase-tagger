d6c0256a9076347360ae1fdbb1a28158356b4143
dexteradeus <dexteradeus@users.noreply.github.com>
2016-07-24 01:21:02 -0400

    Enable thread config in roundtrip.sh

--- roundtrip.sh
+++ roundtrip.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 COUNT_TRAIN=20000
 COUNT_TEST=2000
+COUNT_THREAD=$(grep -c ^processor /proc/cpuinfo)
 
 usage() {
-    printf "Usage: %s: [−-train-count COUNT] [−test-count COUNT]\n" $0
+    printf "Usage: %s: [−-train-count COUNT] [−-test-count COUNT] [--thread-count COUNT]\n" $0
     exit 2
 }
 
@@ -20,6 +21,10 @@ while getopts "$optspec" optchar; do
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
                     COUNT_TEST=$val
                     ;;
+                "threads")
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    COUNT_THREAD=$val
+                    ;;
                 *)
                     usage;;
             esac;;
@@ -32,13 +37,13 @@ echo "Training count: ${COUNT_TRAIN}"
 echo "Testing count: ${COUNT_TEST}"
 
 echo "generating training data..."
-bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TRAIN --offset=0 > tmp/train_file || exit 1
+bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TRAIN --offset=0 --threads=$COUNT_THREAD > tmp/train_file || exit 1
 
 echo "generating test data..."
-bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TEST --offset=$COUNT_TRAIN > tmp/test_file || exit 1
+bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TEST --offset=$COUNT_TRAIN --threads=$COUNT_THREAD > tmp/test_file || exit 1
 
 echo "training..."
-crf_learn template_file tmp/train_file tmp/model_file || exit 1
+crf_learn -p$COUNT_THREAD template_file tmp/train_file tmp/model_file || exit 1
 
 echo "testing..."
 crf_test -m tmp/model_file tmp/test_file > tmp/test_output || exit 1
