9572444d05c88e3bc97170529367c458e4836fbb
dexteradeus <dexteradeus@users.noreply.github.com>
2016-07-26 15:25:03 -0400

    Allow data file to be provided to roundtrip.sh

--- roundtrip.sh
+++ roundtrip.sh
@@ -4,7 +4,7 @@ COUNT_TEST=2000
 COUNT_THREAD=$(grep -c ^processor /proc/cpuinfo)
 
 usage() {
-    printf "Usage: %s: [−-train-count COUNT] [−-test-count COUNT] [--thread-count COUNT]\n" $0
+    printf "Usage: %s: [−-train-count COUNT] [−-test-count COUNT] [--thread-count COUNT] <input-file>\n" $0
     exit 2
 }
 
@@ -32,15 +32,22 @@ while getopts "$optspec" optchar; do
             usage;;
     esac
 done
+shift $(($OPTIND - 1))
+input_file=$1
+if [ -z "$input_file" ]; then
+    printf "Please provide an input csv file with training data\n"
+    usage
+fi
 
+echo "Input file: ${iput_file}"
 echo "Training count: ${COUNT_TRAIN}"
 echo "Testing count: ${COUNT_TEST}"
 
 echo "generating training data..."
-bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TRAIN --offset=0 --threads=$COUNT_THREAD > tmp/train_file || exit 1
+bin/generate_data --data-path=$input_file --count=$COUNT_TRAIN --offset=0 --threads=$COUNT_THREAD > tmp/train_file || exit 1
 
 echo "generating test data..."
-bin/generate_data --data-path=nyt-ingredients-snapshot-2015.csv --count=$COUNT_TEST --offset=$COUNT_TRAIN --threads=$COUNT_THREAD > tmp/test_file || exit 1
+bin/generate_data --data-path=$input_file --count=$COUNT_TEST --offset=$COUNT_TRAIN --threads=$COUNT_THREAD > tmp/test_file || exit 1
 
 echo "training..."
 crf_learn -p$COUNT_THREAD template_file tmp/train_file tmp/model_file || exit 1
